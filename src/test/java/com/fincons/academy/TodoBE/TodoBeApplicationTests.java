4c0ca6bedd139e3730fae5d8112c7db5b4835b82
Tiziana <tiziana.tabiadon@finconsgroup.com>
2022-05-26 08:38:16 +0200

    Project creation

---
+++ src/test/java/com/fincons/academy/TodoBE/TodoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fincons.academy.TodoBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
